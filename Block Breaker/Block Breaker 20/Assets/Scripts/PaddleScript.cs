69a64c70b01a9633061cebd5b704bc483114f016
Jake Matthew Salerno <a100430@mcast.org.mt>
2017-11-07 12:28:39 +0100

    -We handled Lose board collision. -Tested out mouse position.

---
+++ Block Breaker/Block Breaker 20/Assets/Scripts/PaddleScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        print(Input.mousePosition.x);
+
+        Vector3 newPaddlePos = new Vector3(Input.mousePosition.x, gameObject.transform.position.y, gameObject.transform.position.z);
+        gameObject.transform.position = newPaddlePos;
+	}
+}
--- Block Breaker/Block Breaker 20/Assets/Scripts/UIController.cs
+++ Block Breaker/Block Breaker 20/Assets/Scripts/UIController.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class UIController : MonoBehaviour {
-
 	// Use this for initialization
 	void Start () {
         
